dcae7ce9060fe73d37857561e2e341997b096684
vicke008@gmail.com <vicke008@gmail.com>
2019-11-16 04:50:31 -0800

    First Commit

---
+++ CRUD/Controllers/CustomerController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD.Models;
+
+namespace CRUD.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            using (CustomerModels cust = new CustomerModels())
+            {
+                return View(cust.customerdetails.ToList());
+            }
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            using (CustomerModels ccs = new CustomerModels())
+            {
+                return View(ccs.customerdetails.Where(x=>x.id== id).FirstOrDefault());
+            }
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(customerdetail customer)
+        {
+            try
+            {
+                using (CustomerModels cst = new CustomerModels())
+                {
+                    cst.customerdetails.Add(customer);
+                    cst.SaveChanges();
+                }
+                    // TODO: Add insert logic here
+
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (CustomerModels ccs = new CustomerModels())
+            {
+                return View(ccs.customerdetails.Where(x => x.id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, customerdetail customer)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (CustomerModels cst = new CustomerModels())
+                {
+                    cst.Entry(customer).State = EntityState.Modified;
+                    cst.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (CustomerModels cmd = new CustomerModels())
+            {
+                return View( cmd.customerdetails.Where(x => x.id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (CustomerModels cd = new CustomerModels())
+                        //modelname
+                {
+                    customerdetail customer = cd.customerdetails.Where(x => x.id == id).FirstOrDefault();
+                    //tablenmame               searching id from table name
+                    cd.customerdetails.Remove(customer);
+                    //table name
+                    cd.SaveChanges();
+
+
+                }
+
+                    // TODO: Add delete logic here
+
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CRUD/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD.Models;
+using System.Data;
+
+
+namespace CRUD.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            user u = new user();
+            return View(u);
+            // user is a class name from user.cs file whilch contains the user table getter and setter
+            //we use the object of user class from user.cs to generate object which contain all the fielfname
+        }
+    
+        [HttpPost]
+        public ActionResult AddOrEdit(user u)
+        {           
+            using (UserEntities ue = new UserEntities())
+            {
+                ue.users.Add(u);
+                ue.SaveChanges();
+            }
+            return View(); //Always remember to return view when adding [HttpGet][HttpPost] manually inside controller otherwise it will thro error in Actionresult methodname*
+            
+        }
+    }
+}
